c53c2104804a231a5a5feaf913c5dc61a82685ca
Anderson Pazin <a.pazin@gmail.com>
2025-01-17 16:05:04 -0300

    Add files via upload

--- Heranca/quadrado/Quadrado.java
+++ Heranca/quadrado/Quadrado.java
@@ -16,6 +16,6 @@ public class Quadrado  extends Retangulo{
 
     @Override
     public String toString() {
-        return "[Quadrado]" + lado;
+        return String.format("[Quadrado] %.2f", lado);
     }
 }
