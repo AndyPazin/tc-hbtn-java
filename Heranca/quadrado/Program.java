2800c570ff0198020aaaf53fd5b3e27a3e27b3bb
Anderson Pazin <a.pazin@gmail.com>
2025-01-17 15:45:32 -0300

    Add files via upload

--- Heranca/quadrado/Program.java
+++ Heranca/quadrado/Program.java
@@ -27,5 +27,6 @@ public class Program {
         System.out.printf("Lado: %.2f\n", quadrado.getLado());
         System.out.printf("Area: %.2f\n", quadrado.area());
         System.out.printf(quadrado.toString() + "\n");
+
     }
 }
