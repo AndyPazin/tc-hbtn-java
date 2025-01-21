6f81ddf64f5fe6476c99da8b830202545b1f6751
Anderson Pazin <a.pazin@gmail.com>
2025-01-21 15:11:12 -0300

    Add files via upload

--- Interfaces/workflow/Email.java
+++ Interfaces/workflow/Email.java
@@ -1,6 +1,6 @@
 public class Email implements CanalNotificacao{
     @Override
     public void notificar(Mensagem mensagem) {
-        System.out.printf("[E-MAIL] {%s} - %s\n", mensagem.getTipoMensagem(), mensagem.getTexto());
+        System.out.printf("[EMAIL] {%s} - %s\n", mensagem.getTipoMensagem(), mensagem.getTexto());
     }
 }
