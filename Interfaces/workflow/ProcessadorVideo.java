df652705aa4051db8d2eece82ba81c27d6c0aba3
Anderson Pazin <a.pazin@gmail.com>
2025-01-21 15:05:36 -0300

    Add files via upload

--- Interfaces/workflow/ProcessadorVideo.java
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessadorVideo {
-    private List<CanalNotificacao> canais;
+    private ArrayList<CanalNotificacao> canais;
 
     public ProcessadorVideo() {
         canais = new ArrayList<>();
