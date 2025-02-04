ded1cb43a2273515b612b2e9c707de71b4e5c3e9
Anderson Pazin <a.pazin@gmail.com>
2025-02-04 19:08:35 -0300

    Add files via upload

--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Pessoa {
     private int codigo;
     private String nome;
@@ -13,7 +15,41 @@ public class Pessoa {
         this.salario = salario;
     }
 
+    public int getCodigo() {
+        return codigo;
+    }
 
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 
     public String getCargo() {
         return cargo;
@@ -21,6 +57,8 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return "[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ " + String.format("%f", salario);
+        Locale.setDefault(new Locale("pt", "BR"));
+        return String.format("[%d] %s %s %d R$ %f", getCodigo(),
+                getNome(), getCargo(), getIdade(), getSalario());
     }
 }
