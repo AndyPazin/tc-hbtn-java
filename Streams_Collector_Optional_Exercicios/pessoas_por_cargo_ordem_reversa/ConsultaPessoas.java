fd333ae91a2401292b2919cab1c92e84f8961b15
Anderson Pazin <a.pazin@gmail.com>
2025-02-04 19:58:19 -0300

    Add files via upload

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -1,16 +1,11 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
+
+import java.util.*;
+import java.util.stream.*;
 
 public class ConsultaPessoas {
 
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
-        return pessoas.stream().collect(Collectors.groupingBy(
-                Pessoa::getCargo,
-                TreeMap::new, // Usamos um TreeMap para manter a ordem reversa
-                Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Pessoa::getNome))) // Ordena as pessoas por nome dentro do TreeSet
-        ));
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
     }
 }
--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -1,5 +1,4 @@
-import java.util.Comparator;
-import java.util.Locale;
+import java.util.*;
 
 public class Pessoa implements Comparable {
     private int codigo;
@@ -20,50 +19,32 @@ public class Pessoa implements Comparable {
         return codigo;
     }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
+    public String getCargo() {
+        return cargo;
     }
 
     public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public double getSalario() {
         return salario;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-    @Override
-    public int compareTo(Object obj) {
-        Pessoa pessoa = (Pessoa) obj;
-        return this.getNome().compareTo(pessoa.getNome());
-    }
     @Override
     public String toString() {
         Locale.setDefault(new Locale("pt", "BR"));
         return String.format("[%d] %s %s %d R$ %f", getCodigo(),
                 getNome(), getCargo(), getIdade(), getSalario());
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        Pessoa pessoa = (Pessoa) obj;
+        return this.getNome().compareTo(pessoa.getNome());
     }
 }
